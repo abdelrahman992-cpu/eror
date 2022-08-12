86b7a057e42513f5fa56fcc4cf45cd347a153b3a
FlutLab.io <>
2022-08-12 19:50:48 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ class Counter extends StatefulWidget {
 }
 
 class _CounterState extends State<Counter> {
-  double val;
+ late double val;
 
   void initState() {
     super.initState();
@@ -57,65 +57,4 @@ class MyApp extends StatelessWidget {
 }
 
 Future<void> main() async {
-  runApp(MyApp());
-import 'package:flutter/material.dart';
-
-class Counter extends StatefulWidget {
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  double val;
-
-  void initState() {
-    super.initState();
-    val = 0;
-  }
-
-  void change() {
-    setState(() {
-      val += 1;
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(child: Text('$val'))),
-            MaterialButton(
-              color: Theme.of(context).primaryColor,
-              child: Text(
-                '  ',
-                style: TextStyle(color: Colors.white),
-              ),
-              onPressed: () => change(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Center(
-        child: Container(
-          child: Counter(),
-        ),
-      ),
-    );
-  }
-}
-
-Future<void> main() async {
-  runApp(MyApp());
-}
+  runApp(MyApp());}
